831789bc55ff6f11cc31243f98b05554404e5a25
gabrielcomassetto <77698431+gabrielcomassetto@users.noreply.github.com>
2023-01-09 09:44:53 -0300

    Recursos HTTP

---
+++ src/main/java/webservices/model/resources/ProdutoResource.java
@@ -0,0 +1,65 @@
+package webservices.model.resources;
+
+import java.util.List;
+
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import webservices.model.domain.Produto;
+import webservices.model.service.ProdutoService;
+
+public class ProdutoResource {
+
+	private ProdutoService service = new ProdutoService();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public List<Produto> getProdutos(){
+		return service.getProdutos();
+		
+	}
+	
+	@GET
+	@Path("{produtoId}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Produto getProduto(@PathParam("produtoId")long id) {
+		return service.getProduto(id);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Produto save(Produto produto) {
+		return service.saveProduto(produto);
+	}
+	
+	@PUT
+	@Path("{produtoId}")
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Produto update(@PathParam("produtoId") long id, Produto produto) {
+		produto.setId(id);
+		return service.updateProduto(produto);
+	}
+	
+	
+	@DELETE
+	@Path("{produtoId}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Produto delete(@PathParam("produtoId") long id, Produto produto) {
+		produto.setId(id);
+		return service.deleteProduto(id);
+	}
+	
+	
+}
